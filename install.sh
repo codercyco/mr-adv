240a3270db2074a58ba168043e9f972e017e8f1e
ςץς๏ <72743681+sl-cyco@users.noreply.github.com>
2020-11-06 06:32:58 +0530

    Update install.sh
    chmod

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pkg update -y ; pkg upgrade -y
 pkg install python -y ; pip install --upgrade pip ; pip install requests
 pkg install ruby -y ; gem install lolcat
-mv advmr ~/../usr/bin/
+mv advmr ~/../usr/bin/ ; chmod +x advmr
 clear ; echo "installation successful"
