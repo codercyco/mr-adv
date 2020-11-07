c8884885a58f57d16f95416f60fdcb2520a00c03
ςץς๏ <72743681+sl-cyco@users.noreply.github.com>
2020-11-07 12:49:06 +0530

    Update install.sh
    change for remove readme and sample gif after clone

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+rm README.md sample.gif
 pkg update -y ; pkg upgrade -y
 pkg install python -y ; pip install --upgrade pip ; pip install requests
 pkg install ruby -y ; gem install lolcat
-mv advmr ~/../usr/bin/ ; chmod +x advmr
+mv advmr ~/../usr/bin/ ; chmod 777 advmr
 clear ; echo "installation successful"
