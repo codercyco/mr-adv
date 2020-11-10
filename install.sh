245bdc75fb37311b91d56bf5a0245c2cc3a47628
ςץς๏ <72743681+sl-cyco@users.noreply.github.com>
2020-11-10 12:18:03 +0530

    Update install.sh
    fix mistake

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ rm README.md sample.gif
 pkg update -y ; pkg upgrade -y
 pkg install python -y ; pip install --upgrade pip ; pip install requests
 pkg install ruby -y ; gem install lolcat
-mv advmr ~/../usr/bin/ ; chmod 777 advmr
+mv advmr ~/../usr/bin/ ; cd ~/../usr/bin/ ; chmod 777 advmr ; cd
 clear ; echo "installation successful"
